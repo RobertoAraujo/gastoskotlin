f6f4137848a6f17f8eee5fd8e7e8da59d5e52473
Roberto Moraes <robertomoraes@lampp-it.com.br>
2022-03-06 15:26:59 -0500

    primeiro commit em kotilin

---
+++ src/main/kotlin/br/gov/ac/sefaz/treinamento/GerenciamentoContas/GerenciamentoContasApplication.kt
@@ -0,0 +1,11 @@
+package br.gov.ac.sefaz.treinamento.GerenciamentoContas
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GerenciamentoContasApplication
+
+fun main(args: Array<String>) {
+	runApplication<GerenciamentoContasApplication>(*args)
+}
---
+++ src/test/kotlin/br/gov/ac/sefaz/treinamento/GerenciamentoContas/GerenciamentoContasApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.gov.ac.sefaz.treinamento.GerenciamentoContas
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GerenciamentoContasApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
